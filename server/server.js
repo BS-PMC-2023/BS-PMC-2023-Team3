2480462e1eb17bb17c0b0562c89773531d5b83e3
efratka <efratka@ac.sce.ac.il>
2023-04-01 18:05:13 +0300

    first server commit
    starting server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const axios = require('axios');
+const app = express();
+const port = 3001;
+const env = require('dotenv')
+
+// DONT DELETE!
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// END DONT DELETE!
+
+
+// DONT DELETE!
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
