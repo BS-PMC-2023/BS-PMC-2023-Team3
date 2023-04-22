640fa8e84a8cb79c90ecc65c977861f015ba6e8e
yovelef <yovelef@ac.sce.ac.il>
2023-04-22 19:42:17 +0300

    BSPMC233-58 BSPMC233-59 create addUser function

--- Users.js
+++ Users.js
@@ -26,3 +26,34 @@ async function login(req, response) {
         }
     })
 }
+
+async function addUser(req, response) {
+    const db = await connection();
+    let userObj = req.body;
+    let sql = `INSERT INTO users (FIRSTNAME, LASTNAME, EMAIL, BIRTHDAY, USERNAME,  PASSWORD) 
+            VALUES(:1,:2, :3, :4, :5, :6)`;
+
+    let values = [
+        userObj.FIRSTNAME,
+        userObj.LASTNAME,
+        userObj.EMAIL,
+        userObj.BIRTHDAY,
+        userObj.USERNAME,
+        userObj.PASSWORD
+    ];
+
+    db.execute(sql, values,  (err, res) => {
+        if (err) {
+            console.log(err);
+            return response.status(400).json({ message: "Existing username or existing email" });
+        } else {
+            console.log(res)
+            if (res.rowsAffected > 0) {
+                return response.json({ message: "New user created successfully" });
+            } else {
+                return response.status(400).json({ message: "Something went wrong" });
+            }
+        }
+    });
+
+}
