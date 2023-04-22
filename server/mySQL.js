aa6a3fc30fe026318f12e765243adbd349291fb5
efratka <efratka@ac.sce.ac.il>
2023-04-22 20:07:49 +0300

    BSPMC233-47-create DB

--- server/mySQL.js
+++ server/mySQL.js
@@ -1,20 +1,11 @@
-const mysql = require('mysql2');
 
-// התחברות למסד נתונים, לא יודע איך להתחבר לאורקל תאכלס.
+const oracledb = require('oracledb');
+oracledb.autoCommit = true;
+const connection = async ()=> (oracledb.getConnection({
+    user          : 'efratt', 
+    password      : '123456',
+    connectString : 'localhost/xe'
+}));
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'admin',
-  password: '1234',
-  database: ''
-});
 
-// Connect to the database
-connection.connect(error => {
-  if (error) throw error;
-  console.log('Successfully connected to the database');
-});
-
-// Export the connection
 module.exports = connection;
