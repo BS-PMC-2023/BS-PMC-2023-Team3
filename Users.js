82d11166fcfe32df9cb29e286421ece794179264
yovelef <yovelef@ac.sce.ac.il>
2023-04-22 19:33:38 +0300

    BSPMC233-43 login function

--- Users.js
+++ Users.js
@@ -11,10 +11,7 @@ async function login(req, response) {
         if (res && res.rows[0]) {
             if (res.rows[0][5]==req.body.username && res.rows[0][6]==req.body.password) {            
                 response.status(200).contentType('application/json').json({
-                    "message": "login ok!",
-                    "data": {
-                        username: res.rows[0].username
-                    },
+                    "message": "login ok!"
                 })
             }
             else {
