522b318a2d1022b9d2aba4577adab6236fd536ae
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2023-05-05 10:07:25 +0300

    Routing pages

--- server/server.js
+++ server/server.js
@@ -7,10 +7,44 @@ const env = require('dotenv');
 const connection = require('./mySQL');
 const users = require("./Controller/Users.js").router;
 const warehouse = require("./Controller/Warehouse.js").router;
+const path = require('path')
 
 app.use(cors());
 
 // Configuring body parser middleware
+
+//? HomePage
+app.use(express.static(path.join(__dirname, '/public/client/HomePage')))
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/client/HomePage/HP.html'));
+});
+
+//? Login Page
+app.use(express.static(path.join(__dirname, '/public/client/LoginPage')))
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/client/LoginPage/Login.html'));
+});
+
+//? Register Page
+app.use(express.static(path.join(__dirname, '/public/client/Register_page')))
+app.get('/signup', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/client/Register_page/Register.html'));
+});
+
+//? Personal Area Page
+app.use(express.static(path.join(__dirname, '/public/client/PersonalErea')))
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/client/PersonalErea/Stu-Lec.html'));
+});
+
+//? Login Page
+app.use(express.static(path.join(__dirname, '/public/client/LoginPage')))
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/client/LoginPage/Login.html'));
+});
+
+// Todo: Add Routes
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/users', users);
