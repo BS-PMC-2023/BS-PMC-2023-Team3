d77cb9988f7a0dc9a6a2266388bd9234a6b72c15
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2023-05-10 22:33:02 +0300

    BSPMC233-113 BSPMC233-150 BSPMC233-115

--- server/public/client/PersonalErea/PA.js
+++ server/public/client/PersonalErea/PA.js
@@ -31,7 +31,10 @@ ChooseProfile();
 async function OrderTable() {
 
   const username = sessionStorage.username
-  var d = document.getElementById("orders");
+  var d = document.getElementById("items");
+  if(d[2] != null){
+    d[2].innerHTML="";
+  }
   //call for get to the url:
   let response = await fetch('http://localhost:3001/orders/getAllOrderItemForUser?USERNAME='+username, {
       //Get
@@ -61,6 +64,7 @@ const itemHtml = `
     <p>${item.RETURN_DATE}:תאריך החזרה</p>
   </div>
       <div class="bio-chart">
+        <button class="button-14" role="button">דווח</button>
         <div style="display: inline; width: 100px; height: 100px">
           <canvas width="100" height="100px"></canvas
           ><input
@@ -106,3 +110,86 @@ const itemHtml = `
 }
 
 OrderTable();
+
+
+async function faultItem() {
+  
+  var d = document.getElementById("items");
+  if(d[2] != null){
+    d[2].innerHTML="";
+  }
+ 
+  //call for get to the url:
+  let response = await fetch('http://localhost:3001/warehouse/watchItemForStatus?STATUS=FAULTY', {
+      //Get
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+  })
+  //get data from backend response as json!
+  let body = await response.json()
+  if(body.message){
+    alert(body.message)
+    return;
+  }
+  //if I dont get a variable called data from the back, something is wrong!
+
+body.forEach((item) => {
+const itemHtml = `
+<div class="row">
+<div class="col-md-6">
+  <div class="panel">
+    <div class="panel-body">
+    <div class="bio-desk">
+    <h4 class="red">${item.NAMEITEM}:שם הפריט</h4>
+    <p>${item.S_N}:מקט הפריט</p>
+  </div>
+      <div class="bio-chart">
+      <button class="button-14" role="button">תקין</button>
+      <button class="button-14" role="button">יצא מכלל שימוש</button>
+        <div style="display: inline; width: 100px; height: 100px">
+          <canvas width="100" height="100px"></canvas
+          ><input
+            class="knob"
+            data-width="100"
+            data-height="100"
+            data-displayprevious="true"
+            data-thickness=".2"
+            value = "16"
+            data-fgcolor="#e06b7d"
+            data-bgcolor="#e8e8e8"
+            style="
+              width: 54px;
+              height: 33px;
+              position: absolute;
+              vertical-align: middle;
+              margin-top: 33px;
+              margin-left: -77px;
+              border: 0px;
+              font-weight: bold;
+              font-style: normal;
+              font-variant: normal;
+              font-stretch: normal;
+              font-size: 20px;
+              line-height: normal;
+              font-family: Arial;
+              text-align: center;
+              color: rgb(224, 107, 125);
+              padding: 0px;
+              -webkit-appearance: none;
+              background: none;
+            "
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`
+
+  d.innerHTML += itemHtml;
+});
+}
+
+faultItem();
