12c12990a5df48a3d26f8da9c5c35a982f180b83
efratka <efratka@ac.sce.ac.il>
2023-05-27 15:11:36 +0300

    BSPMC233-183-BSPMC233-167

---
+++ server/Controller/Notification.js
@@ -0,0 +1,15 @@
+const connection = require('../mySQL');
+const express = require('express');
+const router = express.Router();
+
+router.get('/getAllNotiForUser', getAllNotiForUser);
+
+
+
+
+
+
+
+
+
+module.exports = { router};
