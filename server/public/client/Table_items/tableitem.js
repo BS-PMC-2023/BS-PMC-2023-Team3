a8bb96816e3ecd85d809052c1bb15c8bd3b9ea5b
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2023-05-29 23:44:38 +0300

    Update tableitem.js

--- server/public/client/Table_items/tableitem.js
+++ server/public/client/Table_items/tableitem.js
@@ -143,7 +143,7 @@ paginatedItems.forEach((item) => {
                             <input type="date" format="DD-MM-YYYY" name="SN" id ="return" required>
 
                             <button type="button" class="btn btn-primary" onclick = "orderI('${item.name}', '${item.s_n}')" >Submit</button>
-                            <button type="button" class="btn btn-danger" onclick="closeForm()">Close</button>
+                            <button type="button" class="btn btn-danger" onclick="closeFormorder()">Close</button>
                         </form>
                         </div>
         </div>
@@ -154,8 +154,7 @@ paginatedItems.forEach((item) => {
 
 
   }
-  else
-  {
+  else if (sessionStorage.title == "Student") {
     document.getElementById("additem").hidden = true;
     const itemHtml = `
     <div class="col-xl-3 col-sm-6" id="${id}">
@@ -187,7 +186,7 @@ paginatedItems.forEach((item) => {
                             <input type="date" format="DD-MM-YYYY" name="SN" id ="return" required>
 
                             <button type="button" class="btn btn-primary" onclick = "orderI('${item.name}', '${item.s_n}')" >Submit</button>
-                            <button type="button" class="btn btn-danger" onclick="closeForm()">Close</button>
+                            <button type="button" class="btn btn-danger" onclick="closeFormorder()">Close</button>
                         </form>
                         </div>
         </div>
@@ -195,6 +194,55 @@ paginatedItems.forEach((item) => {
     </div>
     </div>`
     d.innerHTML += itemHtml;
+  }
+  else{
+    document.getElementById("additem").hidden = true;
+    const itemHtml = `
+    <div class="col-xl-3 col-sm-6" id="${id}">
+    <div class="card">
+        <div class="card-body">
+        <div class="d-flex align-items-center">
+            <div><img src="" alt="" class="avatar-md rounded-circle img-thumbnail" /></div>
+            <div class="flex-1 ms-3">
+            <h5 class="font-size-16 mb-1" id="name-item"><a href="#" class="text-dark">${item.name}</a></h5>
+            <span class="badge badge-soft-success mb-0" id="cat">${item.category}</span>
+            </div>
+        </div>
+        <div class="mt-3 pt-1">
+            <p class="text-muted mb-0" id="status"><i class="font-size-15 align-middle pe-2 text-primary"></i>זמין</p>
+            <p class="text-muted mb-0 mt-2" id="amount"><i class="font-size-15 align-middle pe-2 text-primary"></i>${item.s_n}</p>
+            <p class="text-muted mb-0 mt-2" id="s-num"><i class="font-size-15 align-middle pe-2 text-primary"></i>${item.amount}</p>
+            <p class="text-muted mb-0 mt-2" id="side"><i class="font-size-15 align-middle pe-2 text-primary"></i>${item.ancillary_items}</p>
+        </div>
+        <div class="d-flex gap-2 pt-4">
+            <button type="button" class="btn btn-danger" id="safe">הוראות בטיחות</button>
+            <button type="button" class="btn btn-primary" onclick="openFormorder()" >השאלה</button>
+            <div>
+                        <div class="form-popup" id="dateForm">
+                        <form class="form-container">
+                            <label for="name"><b> תאריך לקיחה </b></label>
+                            <input type="date" format="DD-MM-YYYY"  value = ${new Date().toLocaleDateString('en-GB').split('/').reverse().join('-')} name="name" id ="borrow" required readonly>
+
+                            <input type="date" format="DD-MM-YYYY" value = ${new Date().toLocaleDateString('en-GB').split('/').reverse().join('-')} name="SN" id ="return" class="hidden-input"  required>
+                            <style>
+                                .hidden-input {
+                                display: none;
+                                }
+                            </style>
+                            
+
+                            <p>יש להחזיר את הפריט עד סוף היום<p>
+
+                            <button type="button" class="btn btn-primary" onclick = "orderI('${item.name}', '${item.s_n}')" >Submit</button>
+                            <button type="button" class="btn btn-danger" onclick= "closeFormorder()" >Close</button>
+                        </form>
+                        </div>
+        </div>
+        </div>
+    </div>
+    </div>`
+    d.innerHTML += itemHtml;
+
   }
 });
 }
@@ -350,6 +398,8 @@ async function NumNotificationUser(){
     }
 }
 
+nextpage();
+table(1);
 
 if(sessionStorage.title == 'StorgeManger')
 {
@@ -364,5 +414,4 @@ else{
 
 
 
-    nextpage();
-    table(1);
+  
