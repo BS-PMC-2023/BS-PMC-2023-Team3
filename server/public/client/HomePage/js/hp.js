68a451136c20db5e57eef583e4f838d1cd08ece2
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2023-05-29 22:55:50 +0300

    BSPMC233-179 BSPMC233-184 BSPMC233-190 BSPMC233-194 BSPMC233-198

--- server/public/client/HomePage/js/hp.js
+++ server/public/client/HomePage/js/hp.js
@@ -5,3 +5,112 @@ document.getElementById('items').addEventListener('click', function() {
         document.querySelector('#content').innerHTML = html;
       });
   });
+
+  async function notificationUser(){
+    username = sessionStorage.username
+    var d = document.getElementById("noti");
+
+    let response = await fetch('http://localhost:3001/notification/getAllNotiForUser?USERNAME='+username, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+        body.forEach((item) => {
+            const itemHtml = `
+            <div class="notify_item">
+            <div class="notify_img">
+            </div>
+            <div class="notify_info">
+              <p>${item.DESCRIPTION}</p>
+            </div>
+          </div>
+            `
+            d.innerHTML += itemHtml;  
+        });
+        
+    }
+}
+
+async function notificationManager(){
+    var d = document.getElementById("noti");
+
+    let response = await fetch('http://localhost:3001/notification/getAllNotiForManager', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+        body.forEach((item) => {
+            const itemHtml = `
+            <div class="notify_item">
+            <div class="notify_img">
+            </div>
+            <div class="notify_info">
+              <p>${item.DESCRIPTION}</p>
+            </div>
+          </div>
+            `
+            d.innerHTML += itemHtml;  
+        });
+        
+    }
+}
+
+async function NumNotificationManager(){
+    var d = document.getElementById("numnoti");
+
+    let response = await fetch('http://localhost:3001/notification/getNumberNotReadNoti', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+            d.innerHTML += body;  
+    }
+}
+
+async function NumNotificationUser(){
+    username = sessionStorage.username
+    var d = document.getElementById("numnoti");
+
+    let response = await fetch('http://localhost:3001/notification/getNumberNotReadNotiForUser?USERNAME='+username, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+            d.innerHTML += body;  
+    }
+}
+
+
+if(sessionStorage.title == 'StorgeManger')
+{
+    NumNotificationManager();
+    notificationManager();
+}
+else{
+    NumNotificationUser();
+    notificationUser();
+}
--- server/public/client/Table_items/tableitem.js
+++ server/public/client/Table_items/tableitem.js
@@ -49,8 +49,12 @@ async function orderI(name , s_n  ){
     if(body.message)
         alert(body.message);
     
-    if(body.status == 200)
+    if(body.status == 200){
         document.getElementById(id).remove();
+
+
+    }
+        
 }
 
 
@@ -247,6 +251,118 @@ async function addI(name,s_n,cat){
     closeForm();
     location.reload();
 }
+
+async function notificationUser(){
+    username = sessionStorage.username
+    var d = document.getElementById("noti");
+
+    let response = await fetch('http://localhost:3001/notification/getAllNotiForUser?USERNAME='+username, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+        body.forEach((item) => {
+            const itemHtml = `
+            <div class="notify_item">
+            <div class="notify_img">
+            </div>
+            <div class="notify_info">
+              <p>${item.DESCRIPTION}</p>
+            </div>
+          </div>
+            `
+            d.innerHTML += itemHtml;  
+        });
+        
+    }
+}
+
+async function notificationManager(){
+    var d = document.getElementById("noti");
+
+    let response = await fetch('http://localhost:3001/notification/getAllNotiForManager', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+        body.forEach((item) => {
+            const itemHtml = `
+            <div class="notify_item">
+            <div class="notify_img">
+            </div>
+            <div class="notify_info">
+              <p>${item.DESCRIPTION}</p>
+            </div>
+          </div>
+            `
+            d.innerHTML += itemHtml;  
+        });
+        
+    }
+}
+
+async function NumNotificationManager(){
+    var d = document.getElementById("numnoti");
+
+    let response = await fetch('http://localhost:3001/notification/getNumberNotReadNoti', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+            d.innerHTML += body;  
+    }
+}
+
+async function NumNotificationUser(){
+    username = sessionStorage.username
+    var d = document.getElementById("numnoti");
+
+    let response = await fetch('http://localhost:3001/notification/getNumberNotReadNotiForUser?USERNAME='+username, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    //get data from backend response as json!
+    let body = await response.json();
+    if(body.message)
+        alert(body.message);
+    else{
+            d.innerHTML += body;  
+    }
+}
+
+
+if(sessionStorage.title == 'StorgeManger')
+{
+    NumNotificationManager();
+    notificationManager();
+}
+else{
+    NumNotificationUser();
+    notificationUser();
+}
+
+
+
 
     nextpage();
     table(1);
