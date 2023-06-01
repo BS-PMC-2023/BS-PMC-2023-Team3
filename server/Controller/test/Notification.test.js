ab70c66169a82129cc19b0e952a802f93723fb97
efratka <efratka@ac.sce.ac.il>
2023-06-01 09:40:06 +0300

    Update Notification.test.js

--- server/Controller/test/Notification.test.js
+++ server/Controller/test/Notification.test.js
@@ -10,3 +10,12 @@ describe("GET /notification/getAllNotiForUser", () => {
         expect(response.statusCode).toBe(200);
     });
 });
+
+describe("GET /notification/getAllNotiForManager", () => {
+    it("It should respond with an array of items", async () => {
+        const response = await request(app).get("/notification/getAllNotiForManager");
+        expect(response.body[0]).toHaveProperty("DESCRIPTION");
+        expect(response.body[0]).toHaveProperty("READ");
+        expect(response.statusCode).toBe(200);
+    });
+});
